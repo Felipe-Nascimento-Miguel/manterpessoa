17057980bd6e81f973bbef3cb16550f537e9803e
Felipe-Nascimento-Miguel <164952960+Felipe-Nascimento-Miguel@users.noreply.github.com>
2025-03-22 10:37:15 -0300

    Create tarefas.js

---
+++ tarefas.js
@@ -0,0 +1,5 @@
+function remover_tarefa ()
+{
+//*Obs: nao foi especificado que eu deveria programar essa parte então só fiz essa marcação,
+//também não era possível alterar tarefas.js porque ele está em outro branch, então assumi que deveria criar tarefas.js novamente porém, no branch master
+}
